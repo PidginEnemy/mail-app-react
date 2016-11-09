8ef31ba49135f0699e125adf9302d02f34a0b606
tolchennikov <tolchennikove@nefrosovet.ru>
2016-11-09 13:59:54 +0300

    rename routeHandlers to containers

--- client/routes.js
+++ client/routes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {Router, Route, IndexRedirect} from 'react-router';
 import history from './history';
 import App from './components/App/App';
-import MailRoot from './routeHandlers/MailRoot/MailRoot';
-import MailsListPage from './routeHandlers/MailsListPage/MailsListPage';
-import MailPage from './routeHandlers/MailPage/MailPage';
-import UsersPage from './routeHandlers/UsersPage/UsersPage';
-import NotFoundPage from './routeHandlers/NotFoundPage/NotFoundPage';
+import MailRoot from './containers/MailRoot/MailRoot';
+import MailsListPage from './containers/MailsListPage/MailsListPage';
+import MailPage from './containers/MailPage/MailPage';
+import UsersPage from './containers/UsersPage/UsersPage';
+import NotFoundPage from './containers/NotFoundPage/NotFoundPage';
 
 export default <Router history={history}>
     <Route path="/" component={App}>
