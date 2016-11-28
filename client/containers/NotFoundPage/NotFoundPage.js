ad24fdface201cf9f259ee082989bfb610c61a97
evgenijtolcennikov <sharpextreem@mail.ru>
2016-11-29 00:57:49 +0300

    add link to main page

--- client/containers/NotFoundPage/NotFoundPage.js
+++ client/containers/NotFoundPage/NotFoundPage.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router';
 import CSSModules from 'react-css-modules';
 import styles from './not-found.module.scss';
 
@@ -9,6 +10,7 @@ const NotFoundPage = (props) => {
         <h1 styleName="not-found-title">Error 404</h1>
         <span styleName="not-found-icon" className="glyphicon glyphicon-alert"></span>
         <h1 styleName="not-found-title">Page not found</h1>
+        <Link to="/">Return to main page</Link>
       </div>
     </div>
   );
