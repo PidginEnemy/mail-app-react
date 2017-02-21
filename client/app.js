e0f3e63b589133c69c74e3796aefa2ae642e2c9d
etolchennikov <e.tolchennikov@netology-group.ru>
2017-02-21 13:38:11 +0300

    install push.js

--- client/app.js
+++ client/app.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import routes from './routes';
 import { install as offlineInstall } from 'offline-plugin/runtime';
+import Push from 'push.js';
 
 render(routes, document.getElementById('app'));
 
 if (process.env.NODE_ENV === 'production') {
   offlineInstall();
 }
+
+window.Push = Push; // TODO develop
